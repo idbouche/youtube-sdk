9bfb263a00358b6660dc13adca648a252ae710d9
MarquesDev <marques.developpement@gmail.com>
2016-08-08 15:34:23 +0200

    refactoring request function + test

--- src/index.js
+++ src/index.js
@@ -1,41 +1,57 @@
+var request        = require('request'),
+    qs             = require('qs'),
+    debug          = require('debug')('app:sdk'),
+    hasOwnProperty = Object.prototype.hasOwnProperty;
 
-var request = require('request');
-
-var hasOwnProperty = Object.prototype.hasOwnProperty;
 function isEmpty(obj) {
-    if (obj == null) return true;
-    if (obj.length > 0)    return false;
-    if (obj.length === 0)  return true;
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
-    return true;
+  if ( obj == null ) return true;
+  if ( obj.length > 0 ) return false;
+  if ( obj.length === 0 ) return true;
+  for (var key in obj) {
+    if ( hasOwnProperty.call(obj, key) ) return false;
+  }
+  return true;
 }
 
-var Youtube = function(method, params) {
-  this.key ;
+var Youtube = function () {
+  var ENDPOINT = 'https://www.googleapis.com/youtube/v3/';
+  var KEY = null;
+
+  var _request = function (method, url, params, callback) {
+    if ( isEmpty(params) ) return callback(new Error('Params empty'));
+    if ( !callback ) return callback(new Error('No callback'));
+
+    var url = ENDPOINT + url + '?' + qs.stringify(params) + '&key=' + KEY;
+
+    debug('URL= %s', url);
+
+    request({ url   : url, method: method.toUpperCase() }, function (err, headers, body) {
+      if ( err ) return callback(err, null, null);
+      var body = JSON.parse(body);
+      if(body.error) return callback(body, null, null);
+
+      return callback(err, body, headers);
+    });
+  }
+
   this.use = function (key) {
-     this.key = key;
+    KEY = key;
   };
 
-  this.get = function (method, params, callback) {
-    if (isEmpty(params)===false && callback){
-      var url = 'https://www.googleapis.com/youtube/v3/'+ method +'?'
-      for (var parm in params){
-        url +=  parm + '=' + params[parm]+ '&';
-      }
-      url += 'order=date&type=video%2Clist&key='+this.key
-      request({
-        url:url,
-        method: 'GET'
-      }, function (err, res, body){
-        body = JSON.parse(body);
-        callback(err,body);
-      });
-    }else{
-      callback({message: 'Error'})
-    }
+  this.get = function (url, params, callback) {
+    return _request('GET', url, params, callback);
+  }
 
+  this.post = function (url, params, callback) {
+    return _request('POST', url, params, callback);
+  }
+
+  this.put = function (url, params, callback) {
+    return _request('PUT', url, params, callback);
+  }
+
+  this.delete = function (url, params, callback) {
+    return _request('DELETE', url, params, callback);
   }
 }
 
